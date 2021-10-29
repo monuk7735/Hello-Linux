4019866af13d61a6162874001a646005ab778b6d
Khushwant Kaswan <56693164+khushwantk@users.noreply.github.com>
2021-10-29 17:33:46 +0530

    Removed Unnecessay Comments-III
    Nothing to say

--- pattern3.cpp
+++ pattern3.cpp
@@ -1,12 +1,6 @@
 #include<iostream>
 using namespace std;
-
-
 int main(){
-
-       /*  Read input as specified in the question.
-	* Print output as specified in the question.
-	*/
     int n;
     cin>>n;
     int i=1;
@@ -22,5 +16,4 @@ int main(){
         cout<<endl;
         i++;
     }
-    
 }
